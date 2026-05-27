90a45eb8e613c8693f5435416fccdc924ed06edf
patchigolla santhosh <santhosh.ramana96@gmail.com>
2026-05-27 09:46:40 +0530

    Roboshop Automation

--- frontend.sh
+++ frontend.sh
@@ -4,6 +4,14 @@ echo "Configuration Management for $COMPONENT $ENVIRONMENT in progress"
 
 # It should run as root user or
 
+ID=$(id -u)
+
+if [ $ID -ne 0 ]; then
+  echo "This script must be run as root"
+  echo "Use: sudo $0"
+  exit 1
+fi
+
 echo "Disable default nginx version"
 dnf module disable nginx -y
 
@@ -13,3 +21,12 @@ dnf module enable nginx:1.24 -y
 echo "Install nginx"
 dnf install nginx -y
 #
+echo "Download the frontend code"
+curl -L -o /tmp/frontend.zip https://stan-robotshop.s3.amazonaws.com/frontend-v3.zip
+
+echo "Perform cleanup and unzip the frontend code"
+cd /usr/share/nginx/html
+rm -rf *
+
+echo "extract the frontend code"
+unzip /tmp/frontend.zip
