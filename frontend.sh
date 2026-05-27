7a413a538d5a01c88f61f55723df2fd9e9d18bda
patchigolla santhosh <santhosh.ramana96@gmail.com>
2026-05-27 10:04:44 +0530

    Roboshop Automation

--- frontend.sh
+++ frontend.sh
@@ -13,27 +13,27 @@ if [ $ID -ne 0 ]; then
 fi
 
 echo "Disable default nginx version"
-dnf module disable nginx -y
+dnf module disable nginx -y &>>/tmp/${COMPONENT}.log
 
 echo "Enable nginx:1.24 version"
-dnf module enable nginx:1.24 -y
+dnf module enable nginx:1.24 -y &>>/tmp/${COMPONENT}.log
 
 echo "Install nginx"
-dnf install nginx -y
+dnf install nginx -y &>>/tmp/${COMPONENT}.log
 #
 echo "Download the $COMPONENT component code"
-curl -L -o /tmp/frontend.zip https://stan-robotshop.s3.amazonaws.com/$COMPONENT-v3.zip
+curl -L -o /tmp/${COMPONENT}.zip https://stan-robotshop.s3.amazonaws.com/$COMPONENT-v3.zip
 
 echo "Perform cleanup and unzip the $COMPONENT code"
-cd /usr/share/nginx/html
-rm -rf *
+cd /usr/share/nginx/html 
+rm -rf * &>>/tmp/${COMPONENT}.log
 
 echo "extract the $COMPONENT component code"
-unzip /tmp/$COMPONENT.zip
+unzip /tmp/$COMPONENT.zip &>>/tmp/${COMPONENT}.log
 
 
 echo "Start $COMPONENT service"
-systemctl enable nginx
-systemctl restart nginx
+systemctl enable nginx &>>/tmp/${COMPONENT}.log
+systemctl restart nginx &>>/tmp/${COMPONENT}.log
 
 echo "Configuration Management for $COMPONENT is completed"
