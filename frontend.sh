2ef2be745f9b1cbcc8445846f743442ddf84f5d3
patchigolla santhosh <santhosh.ramana96@gmail.com>
2026-05-27 10:09:56 +0530

    Roboshop Automation

--- frontend.sh
+++ frontend.sh
@@ -5,6 +5,7 @@ echo "Configuration Management for $COMPONENT $ENVIRONMENT in progress"
 # It should run as root user or
 ID=$(id -u)
 COMPONENT="frontend"
+LOG= "/tmp/${COMPONENT}.log"
 
 if [ $ID -ne 0 ]; then
   echo "This script must be run as root"
@@ -13,27 +14,27 @@ if [ $ID -ne 0 ]; then
 fi
 
 echo "Disable default nginx version"
-dnf module disable nginx -y &>>/tmp/${COMPONENT}.log
+dnf module disable nginx -y  &>> $LOG
 
 echo "Enable nginx:1.24 version"
-dnf module enable nginx:1.24 -y &>>/tmp/${COMPONENT}.log
+dnf module enable nginx:1.24 -y &>> $LOG
 
 echo "Install nginx"
-dnf install nginx -y &>>/tmp/${COMPONENT}.log
+dnf install nginx -y &>> $LOG
 #
 echo "Download the $COMPONENT component code"
 curl -L -o /tmp/${COMPONENT}.zip https://stan-robotshop.s3.amazonaws.com/$COMPONENT-v3.zip
 
 echo "Perform cleanup and unzip the $COMPONENT code"
 cd /usr/share/nginx/html 
-rm -rf * &>>/tmp/${COMPONENT}.log
+rm -rf * &>> $LOG
 
 echo "extract the $COMPONENT component code"
-unzip /tmp/$COMPONENT.zip &>>/tmp/${COMPONENT}.log
+unzip /tmp/$COMPONENT.zip &>> $LOG
 
 
 echo "Start $COMPONENT service"
-systemctl enable nginx &>>/tmp/${COMPONENT}.log
-systemctl restart nginx &>>/tmp/${COMPONENT}.log
+systemctl enable nginx &>> $LOG
+systemctl restart nginx &>> $LOG
 
 echo "Configuration Management for $COMPONENT is completed"
