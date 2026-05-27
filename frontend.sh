87da7fecc90b48fefb98e1b909d70034006f4570
patchigolla santhosh <santhosh.ramana96@gmail.com>
2026-05-27 09:56:48 +0530

    Roboshop Automation

--- frontend.sh
+++ frontend.sh
@@ -3,8 +3,8 @@
 echo "Configuration Management for $COMPONENT $ENVIRONMENT in progress"
 
 # It should run as root user or
-
 ID=$(id -u)
+COMPONENT="frontend"
 
 if [ $ID -ne 0 ]; then
   echo "This script must be run as root"
@@ -21,19 +21,19 @@ dnf module enable nginx:1.24 -y
 echo "Install nginx"
 dnf install nginx -y
 #
-echo "Download the frontend code"
-curl -L -o /tmp/frontend.zip https://stan-robotshop.s3.amazonaws.com/frontend-v3.zip
+echo "Download the $COMPONENT component code"
+curl -L -o /tmp/frontend.zip https://stan-robotshop.s3.amazonaws.com/$COMPONENT-v3.zip
 
-echo "Perform cleanup and unzip the frontend code"
+echo "Perform cleanup and unzip the $COMPONENT code"
 cd /usr/share/nginx/html
 rm -rf *
 
-echo "extract the frontend code"
-unzip /tmp/frontend.zip
+echo "extract the $COMPONENT component code"
+unzip /tmp/$COMPONENT.zip
 
 
-echo "Start nginx service"
-systemctl enable nginx
-systemctl restart nginx
+echo "Start $COMPONENT service"
+systemctl enable $COMPONENT
+systemctl restart $COMPONENT
 
-echo "Configuration Management for frontend is completed"
+echo "Configuration Management for $COMPONENT is completed"
