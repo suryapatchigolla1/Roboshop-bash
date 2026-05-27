394bd86f40c50a779bc21c44c4d4da5ee7875c12
patchigolla santhosh <santhosh.ramana96@gmail.com>
2026-05-27 10:23:22 +0530

    Roboshop Automation

--- frontend.sh
+++ frontend.sh
@@ -15,26 +15,67 @@ fi
 
 echo "Disable default nginx version"
 dnf module disable nginx -y  &>> $LOG
+if [ $? -ne 0 ]; then
+    echo "Pass"
+else    
+    echo "Fail"    
+    exit 2
+fi
 
 echo "Enable nginx:1.24 version"
 dnf module enable nginx:1.24 -y &>> $LOG
+if [ $? -ne 0 ]; then
+    echo "Pass"
+else    
+    echo "Fail"    
+    exit 2
+fi
 
 echo "Install nginx"
 dnf install nginx -y &>> $LOG
-#
+if [ $? -ne 0 ]; then
+    echo "Pass"
+else    
+    echo "Fail"    
+    exit 2
+fi
+
 echo "Download the $COMPONENT component code"
 curl -L -o /tmp/${COMPONENT}.zip https://stan-robotshop.s3.amazonaws.com/$COMPONENT-v3.zip
+if [ $? -ne 0 ]; then
+    echo "Pass"
+else    
+    echo "Fail"    
+    exit 2
+fi
 
 echo "Perform cleanup and unzip the $COMPONENT code"
 cd /usr/share/nginx/html 
 rm -rf * &>> $LOG
+if [ $? -ne 0 ]; then
+    echo "Pass"
+else    
+    echo "Fail"    
+    exit 2
+fi
 
 echo "extract the $COMPONENT component code"
 unzip /tmp/$COMPONENT.zip &>> $LOG
-
+if [ $? -ne 0 ]; then
+    echo "Pass"
+else    
+    echo "Fail"    
+    exit 2
+fi
 
 echo "Start $COMPONENT service"
 systemctl enable nginx &>> $LOG
 systemctl restart nginx &>> $LOG
+if [ $? -ne 0 ]; then
+    echo "Pass"
+else    
+    echo "Fail"    
+    exit 2
+fi
 
 echo "Configuration Management for $COMPONENT is completed"
