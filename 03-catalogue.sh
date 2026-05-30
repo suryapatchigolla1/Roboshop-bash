e4b2da2034756df830d0f34eb25c73dbc6f6c750
patchigolla santhosh <santhosh.ramana96@gmail.com>
2026-05-30 22:31:16 +0530

    Roboshop Automation

--- 03-catalogue.sh
+++ 03-catalogue.sh
@@ -5,67 +5,26 @@ COMPONENT="catalogue"
 
 source ./common.sh
 
+nodejs
 
-echo -n "Disabling Node.js module: "
-dnf module disable nodejs -y &>> $LOG
-stat $?
 
-echo -n "Enabling Node.js 20 module: "
-dnf module enable nodejs:20 -y &>> $LOG 
-stat $?
 
-echo -n "Installing Node.js: "
-dnf install nodejs -y &>> $LOG
-stat $?
 
-echo -n "creating user add roboshop: "
-id $APPUSER &>/dev/null || useradd $APPUSER
-stat $?
 
-echo -n "performing cleanup: "
-rm -rf /app || true &>> $LOG
-stat $?
 
-echo -n "creating APP directory: "
-mkdir -p /app &>> $LOG
-stat $?
 
-echo -n "Downloading the $COMPONENT component code: "
-curl -o /tmp/$COMPONENT.zip https://stan-robotshop.s3.amazonaws.com/${COMPONENT}-v3.zip &>> $LOG
-stat $?
 
-echo -n "Copying $COMPONENT systemd file: "
-cp "$SCRIPT_DIR/${COMPONENT}.service" /etc/systemd/system/${COMPONENT}.service
-stat $?
 
-echo -n "Extracting the $COMPONENT code: "
-unzip -o /tmp/${COMPONENT}.zip -d /app &>> $LOG
-stat $?
 
-echo -n "Generation $COMPONENT Artifact: "
 
-cd /app && npm install &>> $LOG
-stat $?
 
-echo -n "Configuring Mongo shell repo: "
-cp "$SCRIPT_DIR/mongo.repo" /etc/yum.repos.d/mongo.repo &>> $LOG
-stat $?
 
-echo "Installation mongodb shell: "
-dnf install mongodb-mongosh -y &>> $LOG
-stat $?
 
-echo -n "Loading $COMPONENT schema: "
-mongosh --host mongodb.robo60.online </app/db/master-data.js &>> $LOG
-stat $?
 
-echo -n "Starting $COMPONENT service: "
-systemctl daemon-reload 
-systemctl enable $COMPONENT &>> $LOG
-systemctl start $COMPONENT &>> $LOG
-stat $?
 
-echo -e "\n \t ___ Configuration Management for $COMPONENT in completed! ___"
+
+
+
 
 
 
--- common.sh
+++ common.sh
@@ -35,9 +35,82 @@ create_user() {
 }
 
 
+download_and_extract() {
+    echo -n "performing cleanup: "
+    rm -rf /app || true &>> $LOG
+    stat $?
 
+    echo -n "creating APP directory: "
+    mkdir -p /app &>> $LOG
+    stat $?
 
+    echo -n "Downloading the $COMPONENT component code: "
+    curl -o /tmp/$COMPONENT.zip https://stan-robotshop.s3.amazonaws.com/${COMPONENT}-v3.zip &>> $LOG
+    stat $?
 
+    echo -n "Extracting the $COMPONENT code: "
+    unzip -o /tmp/$COMPONENT.zip -d /app &>> $LOG
+    stat $?
+}
+
+config_svc() {
+    echo -n "Copying $COMPONENT systemd file: "
+    cp "$SCRIPT_DIR/${COMPONENT}.service" /etc/systemd/system/${COMPONENT}.service
+    stat $?
+
+    echo -n "Starting $COMPONENT service: "
+    systemctl daemon-reload 
+    systemctl enable $COMPONENT &>> $LOG
+    systemctl start $COMPONENT &>> $LOG
+    stat $?
+}
+
+install_mongo_shell() {
+    echo -n "Configuring Mongo shell repo: "
+    cp "$SCRIPT_DIR/mongo.repo" /etc/yum.repos.d/mongo.repo &>> $LOG
+    stat $?
+
+    echo "Installation mongodb shell: "
+    dnf install mongodb-mongosh -y &>> $LOG
+    stat $?
+}
+
+nodejs() {
+    echo -n "Disabling Node.js module: "
+    dnf module disable nodejs -y &>> $LOG
+    stat $?
+
+    echo -n "Enabling Node.js 18.x: "
+    dnf module enable nodejs:18 -y &>> $LOG
+    stat $?
+
+    echo -n "Installing Node.js: "
+    dnf install nodejs -y &>> $LOG
+    stat $?
+
+    create_user #source ./common.sh
+
+    download_app
+
+    install_mongo_shell
+
+    download_and_extract
+
+    echo -n "Generation $COMPONENT Artifact: "
+    npm install --prefix ./app &>> $LOG
+    stat $?
+   
+    if [ $COMPONENT == "catalogue" ]; then    
+        echo -n "Loading $COMPONENT schema: "
+        mongosh --host mongodb.robo60.online </app/db/master-data.js &>> $LOG
+        stat $?
+    fi  
+
+    config_svc
+
+    echo -e "\n \t ___ Configuration Management for $COMPONENT in completed! ___"
+
+}
 
 
 
