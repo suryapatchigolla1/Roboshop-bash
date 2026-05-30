f19d9780c54d5d41f096a546e4801adbc89e8f1c
patchigolla santhosh <santhosh.ramana96@gmail.com>
2026-05-30 16:37:12 +0530

    Roboshop Automation

--- 03-catalogue.sh
+++ 03-catalogue.sh
@@ -1,29 +1,10 @@
 #!/bin/bash
-
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
-
 echo "Configuration Management for $COMPONENT $ENVIRONMENT in progress"
-
-ID=$(id -u)
 COMPONENT="catalogue"
-APPUSER="roboshop"
-LOG="/tmp/${COMPONENT}.log"
 
+source common.sh
 
-if [ $ID -ne 0 ]; then
-    echo "This script must be run as root"
-    echo "Use: sudo $0"
-    exit 1
-fi
-
-stat() {
-    if [ $1 -eq 0 ]; then 
-        echo -e "\e[32m Success \e[0m"
-    else
-        echo -e "\e[33m Failure \e[0m "
-        exit 2
-    fi 
-}
 
 echo -n "Disabling Node.js module: "
 dnf module disable nodejs -y &>> $LOG
---
+++ common.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "Configuration Management for $COMPONENT $ENVIRONMENT in progress"
+
+if [ $ID -ne 0 ]; then
+    echo "This script must be run as root"
+    echo "Use: sudo $0"
+    exit 1
+fi
+
+ID=$(id -u)
+APPUSER="roboshop"
+LOG="/tmp/${COMPONENT}.log"
+
+stat() {
+    if [ $1 -eq 0 ]; then 
+        echo -e "\e[32m Success \e[0m"
+    else
+        echo -e "\e[33m Failure \e[0m "
+        exit 2
+    fi 
+}
