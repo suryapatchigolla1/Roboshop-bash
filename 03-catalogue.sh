1f9d226b719aeb6171df0992d7391ae36d84c14a
patchigolla santhosh <santhosh.ramana96@gmail.com>
2026-05-30 16:58:38 +0530

    Roboshop Automation

--- 03-catalogue.sh
+++ 03-catalogue.sh
@@ -3,7 +3,7 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 echo "Configuration Management for $COMPONENT $ENVIRONMENT in progress"
 COMPONENT="catalogue"
 
-source common.sh
+source ./common.sh
 
 
 echo -n "Disabling Node.js module: "
